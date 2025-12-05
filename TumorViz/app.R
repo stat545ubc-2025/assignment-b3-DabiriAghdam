6a0e498b54804df3879d74c0c66f0c4f1d7f97cc
Amirhossein Dabiriaghdam <dabiri.aghdam@gmail.com>
2025-12-04 22:18:30 -0800

    Clean up code, load the dataset and improve description

--- TumorViz/app.R
+++ TumorViz/app.R
@@ -1,47 +1,44 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
 library(shiny)
+library(datateachr)
 
-# Define UI for application that draws a histogram
+# Create a cleaner dataset (adapted from mini-data-analysis repository)
+cancer_data <- cancer_sample %>%
+  select(ID, diagnosis, radius_mean, texture_mean, area_mean, perimeter_mean, smoothness_mean, compactness_mean) %>%
+  mutate(area_to_radius_ratio = area_mean / radius_mean) %>%
+  mutate(texture_mean_category = case_when(
+    texture_mean < quantile(texture_mean, 1/3) ~ "Low",
+    texture_mean < quantile(texture_mean, 2/3) ~ "Medium",
+    TRUE ~ "High"
+  ) %>% factor(levels = c("Low", "Medium", "High"), ordered = TRUE)) %>%
+  mutate(diagnosis = factor(diagnosis, 
+                            levels = c("B", "M"),
+                            labels = c("B", "M"))) %>%
+  filter(!if_any(everything(), is.na)) %>%
+  distinct()
+
+# Define UI for application
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("TumorViz: Cancer Tumor Explorer"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-          h3("TumorViz"),
-          p("An interactive app to explore cancer tumor characteristics and diagnosis."),
-        )
+  
+  # Application title
+  titlePanel("TumorViz: Cancer Tumor Explorer"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      
+    ),
+    
+   
+    mainPanel(
+      h3("TumorViz"),
+      p("TumorViz is an interactive visualization tool for exploring a cleaned subset of Wisconsin Breast Cancer dataset (available in `datateachr` package). The app allows you to investigate relationships between various tumor characteristics (radius, area, etc.) and diagnosis outcomes (benign or malignant)."),
     )
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  
 }
 
 # Run the application 
